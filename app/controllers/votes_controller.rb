ee79510f7e67a0ca297a62a37817c31dd7c71385
Apprentice <apprentice@devbootcamp.com>
2012-07-24 15:25:48 -0700

    add upvote/downvote capability on links not created by the user

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -5,13 +5,13 @@ class VotesController < ApplicationController
   def create
     value = params[:type] == "up" ? 1 : -1
     @link = Link.find(params[:id])
-    @link.add_or_update_evaluation(:votes, value, current_user)
-    redirect_to :back, notice: "Thank you for voting"
-  end
-
-  def show
-    @link = Link.find(params[:id])
-    redirect_to :back, notice: "Thank you for voting"
+    if @link.user_id == current_user.id
+      flash[:error] = "You can't vote on your own links!"
+      redirect_to root_path
+    else
+        @link.add_or_update_evaluation(:votes, value, current_user)
+        redirect_to :back, notice: "Thank you for voting"
+    end
   end
 
 end
