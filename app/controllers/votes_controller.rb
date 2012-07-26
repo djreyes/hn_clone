192ed1591baac8e58a8d314355d81ae805eeec65
Apprentice <apprentice@devbootcamp.com>
2012-07-25 17:28:10 -0700

    added voting to all comments

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -4,15 +4,29 @@ class VotesController < ApplicationController
 
   def create
     value = params[:type] == "up" ? 1 : -1
-    @link = Link.find(params[:id])
-    if current_user == nil
-      redirect_to new_user_session_path
-    elsif @link.user_id == current_user.id
-      flash[:error] = "You can't vote on your own links!"
-      redirect_to root_path
+
+    if params[:class] == "Comment"
+      @comment = Comment.find(params[:id])
+      if current_user == nil
+        redirect_to new_user_session_path
+      elsif @comment.user_id == current_user.id
+        flash[:error] = "You can't vote on your own links!"
+        redirect_to root_path
+      else
+        @comment.add_or_update_evaluation(:votes, value, current_user)
+        redirect_to :back, notice: "Thank you for voting"
+      end
     else
+      @link = Link.find(params[:id])
+      if current_user == nil
+        redirect_to new_user_session_path
+      elsif @link.user_id == current_user.id
+        flash[:error] = "You can't vote on your own links!"
+        redirect_to root_path
+      else
         @link.add_or_update_evaluation(:votes, value, current_user)
         redirect_to :back, notice: "Thank you for voting"
+      end
     end
   end
 
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,4 +4,6 @@ class Comment < ActiveRecord::Base
   belongs_to :commentable, :polymorphic => true
 
   has_many :comments, :as => :commentable
+
+  has_reputation :votes, source: :user, aggregated_by: :sum
 end
